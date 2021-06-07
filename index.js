178d7cd1ecc3c2e2930bb99e412b460c1be2eaae
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-07 21:23:18 +0700

    Tugas 4 js
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,20 @@
+var ersan = 175;
+var fernando = 168;
+var samjaya = 172;
+
+if(ersan>fernando){
+  if(ersan>samjaya){
+    console.log("Ersan memiliki tubuh yang tertinggi");
+  }else{
+    consle.log("Ersan memiliki tinggi badan paling tinggi kedua");
+  }
+}else{
+  if(ersan<samjaya){
+    console.log("Ersan memiliki tinggi badan terpendek")
+  }else{
+    console.log("Ersan memiliki tubuh paling pendek kedua")
+  }
+}
+// Ersan memiliki tubuh tertinggi pertama
+// fernando memiliki tubuh paling tertinggi
+// samjaya memiliki tubuh terpendek
